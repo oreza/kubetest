ea9834e8f0f263caf3ca77bc28f5d1c53a654dab
oreza <p4r1tyb1t@gmail.com>
2018-06-21 11:06:42 -0400

    Remove binary

--- kubetest.go
+++ kubetest.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("request from %v\n", r.RemoteAddr)
-		w.Write([]byte("hello form 1 & 0 \n"))
+		w.Write([]byte("Hello form 1 & 0 \n"))
 	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request from %v\n", r.RemoteAddr)
+		w.Write([]byte("I am healthy \n"))
+	})
+
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
